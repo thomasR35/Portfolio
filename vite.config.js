72c8d9394bf3307893f7937596b0a5e8a03e446b
thomasR35 <thomasriou.35@gmail.com>
2026-03-09 18:59:25 +0100

    remove github pages config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: "./index.html",
-        404: "./404.html",
-      },
-    },
-  },
 });

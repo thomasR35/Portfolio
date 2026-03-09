2d661945df1d795253814d557363c8f2d3f99027
thomasR35 <thomasriou.35@gmail.com>
2026-03-09 09:18:44 +0100

    Refresh Mini Patch

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio/",
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false,
+  base: "/Portfolio/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        404: "./404.html",
+      },
     },
   },
 });

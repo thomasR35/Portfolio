384bc6745e1bf0fe7351d0a5d06fff6ad46d5b43
thomasR35 <thomasriou.35@gmail.com>
2026-03-12 12:03:34 +0100

    Update ProjectDetails.jsx

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -6,6 +6,7 @@ import "../styles/pages/_projectDetails.scss";
 import { Link } from "react-router-dom";
 import SwiperCore from "swiper";
 import { useProject } from "../hooks/useProjectDetails";
+import AnimatedBlock from "../components/AnimatedBlock";
 
 SwiperCore.use([Navigation, Autoplay]);
 
@@ -20,13 +21,59 @@ function ProjectDetails() {
     <div className="project-details">
       <h1>{project.title}</h1>
 
+      {project.label && (
+        <p className="project-details__label">{project.label}</p>
+      )}
+      {project.description && (
+        <p className="project-details__description">
+          {Array.isArray(project.description)
+            ? project.description.map((line, i) => (
+                <span key={i}>
+                  {line}
+                  {i < project.description.length - 1 && (
+                    <>
+                      <br />
+                      <br />
+                    </>
+                  )}
+                </span>
+              ))
+            : project.description}
+        </p>
+      )}
+
+      {project.tags && project.tags.length > 0 && (
+        <div className="project-details__tags">
+          {project.tags.map((tag) => (
+            <span key={tag} className="project-details__tag">
+              {tag}
+            </span>
+          ))}
+        </div>
+      )}
+
+      {project.links && project.links.length > 0 && (
+        <div className="project-details__links">
+          {project.links.map((link) => (
+            <a
+              key={link.label}
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="project-details__link"
+            >
+              {link.label} →
+            </a>
+          ))}
+        </div>
+      )}
+
       <Swiper
         spaceBetween={10}
         slidesPerView={1}
         navigation
         autoplay={{ delay: 3000 }}
         loop={true}
-        loopedSlides={project.images.length}
       >
         {project.images.map((image, index) => (
           <SwiperSlide key={index}>
@@ -38,6 +85,14 @@ function ProjectDetails() {
           </SwiperSlide>
         ))}
       </Swiper>
+
+      {project.blocks && project.blocks.length > 0 && (
+        <ul className="blocks-list">
+          {project.blocks.map((block, index) => (
+            <AnimatedBlock key={index} block={block} />
+          ))}
+        </ul>
+      )}
 
       <div className="back-button-container">
         <Link to="/projects" className="back-button">
